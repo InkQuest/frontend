cd77ecafa43cc56474ce98e8d9e995eda3b8673c
huypham <huyp26102000@gmail.com>
2023-09-20 08:13:25 +0700

    init theme

---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+const customTheme = {
+  components: {
+    Button: {
+      colorPrimary: "#EC6F66",
+      algorithm: true, // Enable algorithm
+    },
+    Input: {
+      colorPrimary: "#EC6F66",
+      algorithm: true, // Enable algorithm
+    },
+  },
+};
+
+export default customTheme;
